7cea5afb0bbc369645aa5b77124060e9e16fab7c
tomelleyz <eleyinmiolasunkanmi@gmail.com>
2025-06-16 06:31:13 +0100

    feat: adjust spotify iframe height, animate header section

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
 
+gsap.registerPlugin(SplitText);
+
+let headerAnimationTimeline = gsap.timeline();
+
+document.fonts.ready.then(() => {
+  let split = SplitText.create(".header__subtitle", {
+    type: "lines",
+    autoSplit: true,
+    onSplit: (self) => {
+      headerAnimationTimeline.to(".header__name__cover", {
+        scaleY: 0,
+        duration: 1,
+        ease: "power4.out",
+      });
+      headerAnimationTimeline.from(
+        ".header__title",
+        { y: 48, autoAlpha: 0 },
+        "-=0.5"
+      );
+      headerAnimationTimeline.from(self.lines, {
+        y: 50,
+        filter: "blur(4px)",
+        autoAlpha: 0,
+        ease: "power4.out",
+        stagger: {
+          amount: 0.25,
+        },
+        // onComplete: () => {
+        //   split.revert();
+        // },
+      });
+    },
+  });
+});
